b989c91eed36890966a5f372bc75f2e5929694c1
Eldio Santos Junior <eldiosantos@gmail.com>
2020-10-05 05:03:29 +0000

    Separating he TCP check from checker.go file

--- health/checker_test.go
+++ health/checker_test.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
 	"net/http/httptest"
@@ -15,94 +14,6 @@ import (
 const (
 	defaultDuration = time.Duration(1 * time.Second)
 )
-
-func TestParseHostHttp(t *testing.T) {
-	h, err := parseHost("http://any-host/abc")
-	if err != nil {
-		t.Errorf("Nao deve retornar erro, mas retornou o seguinte\n%s", err.Error())
-	}
-
-	if h != "any-host:80" {
-		t.Errorf("Deveria retornar o host 'any-host:80', mas retornou '%s'", h)
-	}
-}
-
-func TestParseHostHttps(t *testing.T) {
-	h, err := parseHost("https://any-host/abc")
-	if err != nil {
-		t.Errorf("Nao deve retornar erro, mas retornou o seguinte\n%s", err.Error())
-	}
-
-	if h != "any-host:443" {
-		t.Errorf("Deveria retornar o host 'any-host:443', mas retornou '%s'", h)
-	}
-}
-
-func TestParseHostHttpCustomPort(t *testing.T) {
-	h, err := parseHost("https://any-host:9999/abc")
-	if err != nil {
-		t.Errorf("Nao deve retornar erro, mas retornou o seguinte\n%s", err.Error())
-	}
-
-	if h != "any-host:9999" {
-		t.Errorf("Deveria retornar o host 'any-host:9999', mas retornou '%s'", h)
-	}
-}
-
-func TestParseHostTargetInvalido(t *testing.T) {
-	h, err := parseHost("any-host")
-	if err == nil {
-		t.Error("Deve retornar erro, mas nao retornou o seguinte")
-	}
-
-	if h != "" {
-		t.Errorf("Deveria retornar o host '', mas retornou '%s'", h)
-	}
-}
-
-func TestTcpTestOK(t *testing.T) {
-	service := startservice(9999, t)
-	defer service.Close()
-	cfg := TCPServiceConfig{
-		name:     "success-test",
-		endpoint: "http://localhost:9999",
-		timeout:  defaultDuration,
-	}
-
-	s := cfg.Test()
-
-	log.Println(s.Details)
-	if s.Status != ServiceStatusOK {
-		t.Errorf("Status should be UP, but was '%s'", s.Status)
-	}
-}
-
-func TestTcpTestHostUnavailable(t *testing.T) {
-	cfg := TCPServiceConfig{
-		name:     "invalid-test",
-		endpoint: "http://abc.xyz:1234",
-		timeout:  defaultDuration,
-	}
-
-	s := cfg.Test()
-	if s.Status != ServiceStatusNOK {
-		t.Errorf("Status deve estar DOWN, mas esta '%s'", s.Status)
-	}
-}
-
-func TestTcpTestTargetInvalido(t *testing.T) {
-	cfg := TCPServiceConfig{
-		name:     "abc.com_invalid-test",
-		endpoint: "abc.com",
-		timeout:  defaultDuration,
-	}
-	s := cfg.Test()
-
-	log.Println(s.Details)
-	if s.Status != ServiceStatusNOK {
-		t.Errorf("Status deve estar DOWN, mas esta '%s'", s.Status)
-	}
-}
 
 func TestCheckerEndpoint(t *testing.T) {
 	service0 := startservice(7777, t)
---
+++ health/tcp.go
@@ -0,0 +1,105 @@
+package health
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/url"
+	"time"
+)
+
+/*
+TCPServiceConfig is the checker config type for TCP checks
+*/
+type TCPServiceConfig struct {
+	name     string
+	endpoint string
+	timeout  time.Duration
+}
+
+/*
+Name returns the test/service name
+*/
+func (cfg *TCPServiceConfig) Name() string {
+	return cfg.name
+}
+
+/*
+Type returns the test/service type (ServiceTypeTCP)
+*/
+func (cfg *TCPServiceConfig) Type() ServiceType {
+	return ServiceTypeTCP
+}
+
+/*
+Endpoint returns the test/service endpoint
+*/
+func (cfg *TCPServiceConfig) Endpoint() string {
+	return cfg.endpoint
+}
+
+/*
+Timeout returns the test/service TCP test timeout
+*/
+func (cfg *TCPServiceConfig) Timeout() time.Duration {
+	return cfg.timeout
+}
+
+/*
+Test returns the test/service status
+*/
+func (cfg *TCPServiceConfig) Test() Status {
+	url, err := parseHost(cfg.Endpoint())
+	if err != nil {
+		return Status{
+			Name:   "cartao-adesao",
+			Status: ServiceStatusNOK,
+			Details: map[string]string{
+				"error": err.Error(),
+			},
+		}
+	}
+
+	start := time.Now()
+	conn, err := net.DialTimeout("tcp", url, cfg.Timeout())
+	if err != nil {
+		log.Println("Something wrong: ", err)
+		return Status{
+			Name:   cfg.Name(),
+			Status: ServiceStatusNOK,
+			Details: map[string]string{
+				"cause": err.Error(),
+			},
+		}
+	}
+	defer func() {
+		conn.Close()
+		log.Println("Connection closed")
+	}()
+	return Status{
+		Name:   cfg.Name(),
+		Status: ServiceStatusOK,
+		Details: map[string]string{
+			"time": time.Since(start).String(),
+		},
+	}
+
+}
+
+func parseHost(target string) (host string, err error) {
+	u, err := url.ParseRequestURI(target)
+	if err != nil {
+		return
+	}
+	var port = u.Port()
+	if port == "" {
+		if u.Scheme == "http" {
+			port = "80"
+		} else if u.Scheme == "https" {
+			port = "443"
+		}
+	}
+	host = fmt.Sprintf("%s:%s", u.Hostname(), port)
+
+	return
+}
